17f0b3cd4427e2a3e16600e88963f0882accbc31
Martin Nilsson <you@example.com>
2020-03-23 13:13:39 +0100

    Clean up and added CSS

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hi this is my distance work application
+
+Welcome to the WEABE (Work Effective And Break Effective) project.
+
+## Setting up the project on your platfrom
+
+This project can run on your local machine and store data in your localstorage or 
+you can set it up on a server and store the data on a database such as mongoDB / SQL / MySQL / Oracle and more.
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 
 const app = express()
-
-const port = env.port || 5000
+const port = 5000
 
 app.get('/times', (req, res) => {
     const times = [
         {
-            createdAt: req.createdTime,
-            startTime: req.startTime,
-            endTime: req.endTime,
+            // createdAt: req.createdTime,
+            // startTime: req.startTime,
+            // endTime: req.endTime,
+
+            createdAt: '2020-03-23T:10:38',
+            startTime: '2020-03-23T:10:39',
+            endTime: '2020-03-23T:10:40',
         }
     ]
     res.json(times)
