681a3a71d2db20a1002652827f618083f926eb83
Martin Nilsson <you@example.com>
2020-03-30 17:38:17 +0200

    Added notes and modal

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Timer from './Timer'
+import Notes from './Notes'
 
 const App = () => {
     
@@ -7,11 +8,13 @@ const App = () => {
         <div className="weabe-content">
             <div className="background"></div>
 
-            <div className="studying-img img"></div>
+            <div className="studying-img background-img"></div>
 
-            <div className="walking-img img"></div>
+            <div className="walking-img background-img"></div>
 
             <Timer />
+
+            <Notes />
         </div>
     )
 }
---
+++ client/src/components/Notes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div className="notes-container">
+            <h3>Notes / Cheklist</h3>
+
+            <ul>
+                <li className="note note1">1</li>
+                <li className="note note2">2</li>
+                <li className="note note3">3</li>
+                <li className="note note4">4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Notes
